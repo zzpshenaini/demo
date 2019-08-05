df51e7235b96432a8a87d94f1693555327bd33de
zzpshenaini <1036856500@qq.com>
2019-08-05 16:52:07 +0800

    2019-8-5

---
+++ javaplay/src/Person.java
@@ -0,0 +1,53 @@
+public class Person {
+    private String id;
+    private String name;
+    private String sex;
+    private String address;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void eat(String food){
+        System.out.println("我正在吃"+food);
+    }
+
+    public void sayHello(){
+        System.out.println("Hello World!!!"+this.name);
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.name="小明";
+        person.eat("noodle");
+        person.sayHello();
+    }
+}
